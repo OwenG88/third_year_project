273b6ec02210842fc5e838ae695d6161bdfb4766
Owen Green <54175200+OwenG88@users.noreply.github.com>
2023-11-21 18:27:49 +0000

    classical rw

---
+++ rw_on_line.py
@@ -0,0 +1,20 @@
+from matplotlib import pyplot as plt 
+import numpy as np
+import random
+
+
+n_trials = 1_000_000
+time_steps = 100
+movements = []
+for i in range(n_trials):
+    movements.append(sum(random.choice([1,-1]) for _ in range(time_steps)))
+
+x = [i for i in range(-time_steps, time_steps+1)]
+y = [movements.count(i) / n_trials for i in x]
+
+#Do a labelled scatter plot
+plt.scatter(x, y)
+plt.xlabel("Position")
+plt.ylabel("Probability")
+plt.title("Probability distribution of a classical random walk")
+plt.show()
